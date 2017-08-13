bc6e9e57963cf52381618afffd22ec0fdc9ec5ef
B Bhardwaj <bbhardwaj102@gmail.com>
2017-08-13 17:38:27 +0530

    Oracle database connected

--- Basic/src/bb/com/jdbcDriver/TypeFourConnection.java
+++ Basic/src/bb/com/jdbcDriver/TypeFourConnection.java
@@ -1,6 +1,43 @@
+/*
+Type 4 Connection
+
+*/
+package bb.com.jdbcDriver;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.SQLException;
 
-public class 
+public class TypeFourConnection{
+	public static void main(String[] args){
+		Connection con = null;
+		Statement stmt = null;
+		
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			String user = "system";
+			String password = "root";
+			
+			con = DriverManager.getConnection(url, user, password);
+			
+			System.out.println("Connection Established");
+		}catch(ClassNotFoundException e){
+			System.out.println("Class not found Error " + e.getMessage());
+		}catch(SQLException e){
+			System.out.println("sql error " + e.getMessage());
+		}finally{
+			
+			if(con != null){
+				try{
+					con.close();
+					System.out.println("Disconnected");
+				}catch(SQLException e){
+					System.out.println("Connection closing error " + e.getMessage());
+				}
+			}
+			
+		}
+	}
+}
